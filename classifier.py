974e7c6738cbe3619204335805a91ead2141a4cb
anirudharc <rcanirudha@gmail.com>
2018-10-26 16:56:42 -0400

    classifier pipeline

--- classifier.py
+++ classifier.py
@@ -17,6 +17,10 @@ import visualize
 
 import torch
 
+# Dataset directory
+DATA_DIR = "/media/bighdd7/arayasam/dataset/charades/" 
+OPFLOW_PATH = "/media/bighdd7/arayasam/dataset/charades/Charades_v1_flow"
+RGB_PATH = "/media/bighdd7/arayasam/dataset/charades/Charades_v1_rgb" #06OKE/"
 
 # Root directory of the project
 ROOT_DIR = os.getcwd()
@@ -69,64 +73,163 @@ class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
                'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
                'teddy bear', 'hair drier', 'toothbrush']
 
-# Load a random image from the images folder
-file_names = next(os.walk(IMAGE_DIR))[2]
-# image = skimage.io.imread(os.path.join(IMAGE_DIR, random.choice(file_names)))
-image = cv2.imread(os.path.join(IMAGE_DIR, random.choice(file_names)))
+# Directory traversal
+for root, dirs, files in os.walk(DATA_DIR, topdown=False):
+    print("######################################")
+    if len(dirs) == 0:
+        print(root)
+        print(files[0:10])
+    print("######################################")
+    # for name in files:
+    #     print(os.path.join(root, name))
+    
+    # for name in dirs:
+    #     print(os.path.join(root, name))
 
-# Run detection
-results = model.detect([image])
+# Input and OD for RGB frame
+path = RGB_PATH + "/0DBQD/0DBQD-000001.jpg"
+image = cv2.imread(path)
+
 cv2.imwrite("./result/input.png", image)
-cv2.imwrite("./result/input_snippet.png", image[10:100, 10:100])
-
+results = model.detect([image])
 r = results[0]
-# # print(type(r['rois'][0]))
-# print(r['masks'].shape[2])
-# # print(r['rois'].shape)
-# # print(r['rois'][0].shape)
-# # print(r['rois'])
+print(r['class_ids'])
 
-diff = image.copy()
+# Input and process optical flow frame
+opflow = cv2.imread(OPFLOW_PATH + "/ESAIY/ESAIY-000142y.jpg")
+inverted_opflow = cv2.bitwise_not(opflow)
+retval, threshold = cv2.threshold(inverted_opflow, 128, 255, cv2.THRESH_BINARY)
+
+cv2.imwrite("./result/opflow.png", opflow)
+cv2.imwrite("./result/inverted_opflow.png", inverted_opflow)
+cv2.imwrite("./result/thresholed_opflow.png", threshold)
+
+# Apply mask on thresholded inverted opflow
+inverted_diff = inverted_opflow
+masked_opflow = opflow
+threshold_diff = threshold
 for idx in range(r['masks'].shape[2]):
 
     name = './result/' + str(idx) + '_my.png'
     cv2.imwrite(name, r['masks'][:,:,idx] * 255)
     
     mask = r['masks'][:,:,idx]
+
     for c in range(3):
-            diff[:, :, c] = np.where(mask == 1,
-                                    diff[:, :, c] * 0,
-                                    diff[:, :, c])
+            threshold_diff[:, :, c] = np.where(mask == 1,
+                                    threshold_diff[:, :, c] * 0,
+                                    threshold_diff[:, :, c])
 
-cv2.imwrite("./result/masked.png", diff)
+    for c in range(3):
+            inverted_diff[:, :, c] = np.where(mask == 1,
+                                    inverted_diff[:, :, c] * 0,
+                                    inverted_diff[:, :, c])
+    
+    for c in range(3):
+            masked_opflow[:, :, c] = np.where(mask == 1,
+                                    masked_opflow[:, :, c] * 0,
+                                    masked_opflow[:, :, c])
 
-avg_color_per_row = np.average(image, axis=0)
-avg_color = np.average(avg_color_per_row, axis=0)
-
-diff_avg_color_per_row = np.average(diff, axis=0)
-diff_avg_color = np.average(diff_avg_color_per_row, axis=0)
-
-print(avg_color, diff_avg_color)
-print(r['class_ids'])
-
-#Break down frames into parts
-height, width = diff.shape[:2]
-print(height, width)
-h_gap = height//3
-w_gap = width//4
-
-count = 1
-for start_row in range(0, height, h_gap):
-    for start_col in range(0, width, w_gap):
-        name = './result/part_' + str(count) + '.png'
-        count += 1
-        print(name)
-        print(start_row, start_row + h_gap, start_col, start_col + w_gap)
-        if (start_row + h_gap) <= height and (start_col + w_gap) <= width:
-            
-            cv2.imwrite(name, image[start_row:start_row+h_gap, start_col:start_col+w_gap])
+cv2.imwrite("./result/masked_invereted_threshold.png", threshold_diff)
+cv2.imwrite("./result/masked_invereted_opflow.png", inverted_diff)
+cv2.imwrite("./result/masked_opflow.png", masked_opflow)
 
 # Visualize result
 visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'],
                             class_names, r['scores'])
-plt.show()
+
+
+#Break down frames into parts
+height, width = threshold.shape[:2]
+h_gap = height//3
+w_gap = width//4
+print(height, h_gap, width, w_gap)
+
+count = 1
+for start_row in range(0, height, h_gap):
+    end_row = start_row + h_gap
+    for start_col in range(0, width, w_gap):
+        end_col = start_col + w_gap
+        name = './result/part_' + str(count) + '.png'
+        count += 1
+
+        print("Part Average " + name  + ":")
+        # print(start_row, end_row, start_col, end_col)
+
+        part_avg_color_per_row = np.average(threshold_diff[start_row:end_row, start_col:end_col], axis=0)
+        part_avg_color = np.average(part_avg_color_per_row, axis=0)
+
+        inv_part_avg_color_per_row = np.average(threshold[start_row:end_row, start_col:end_col], axis=0)
+        inv_part_avg_color = np.average(inv_part_avg_color_per_row, axis=0)
+
+        print("Inverted avg:", end =" ")
+        print(inv_part_avg_color)
+        print("Greyscale avg:", end =" ")
+        print(part_avg_color)
+        print()
+
+        if (start_row + h_gap) <= height and (start_col + w_gap) <= width:  
+            cv2.imwrite(name, threshold_diff[start_row:end_row, start_col:end_col])
+
+
+# # Load a random image from the images folder
+# file_names = next(os.walk(IMAGE_DIR))[2]
+# # image = skimage.io.imread(os.path.join(IMAGE_DIR, random.choice(file_names)))
+# image = cv2.imread(os.path.join(IMAGE_DIR, random.choice(file_names)))
+
+# # Run detection
+# results = model.detect([image])
+# cv2.imwrite("./result/input.png", image)
+# cv2.imwrite("./result/input_snippet.png", image[10:100, 10:100])
+
+# r = results[0]
+# # # print(type(r['rois'][0]))
+# # print(r['masks'].shape[2])
+# # # print(r['rois'].shape)
+# # # print(r['rois'][0].shape)
+# # # print(r['rois'])
+
+# diff = image.copy()
+# for idx in range(r['masks'].shape[2]):
+
+#     name = './result/' + str(idx) + '_my.png'
+#     cv2.imwrite(name, r['masks'][:,:,idx] * 255)
+    
+#     mask = r['masks'][:,:,idx]
+#     for c in range(3):
+#             diff[:, :, c] = np.where(mask == 1,
+#                                     diff[:, :, c] * 0,
+#                                     diff[:, :, c])
+
+# cv2.imwrite("./result/masked.png", diff)
+
+# avg_color_per_row = np.average(image, axis=0)
+# avg_color = np.average(avg_color_per_row, axis=0)
+
+# diff_avg_color_per_row = np.average(diff, axis=0)
+# diff_avg_color = np.average(diff_avg_color_per_row, axis=0)
+
+# print(avg_color, diff_avg_color)
+# print(r['class_ids'])
+
+# #Break down frames into parts
+# height, width = diff.shape[:2]
+# print(height, width)
+# h_gap = height//3
+# w_gap = width//4
+
+# count = 1
+# for start_row in range(0, height, h_gap):
+#     for start_col in range(0, width, w_gap):
+#         name = './result/part_' + str(count) + '.png'
+#         count += 1
+#         print(name)
+#         print(start_row, start_row + h_gap, start_col, start_col + w_gap)
+#         if (start_row + h_gap) <= height and (start_col + w_gap) <= width:
+            
+#             cv2.imwrite(name, image[start_row:start_row+h_gap, start_col:start_col+w_gap])
+
+# # Visualize result
+# visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'],
+#                             class_names, r['scores'])
+# plt.show()
